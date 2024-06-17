06426f9e74fe747cad46509013eb285163e9d37d
ehumba <josef.roessl@gmx.at>
2024-06-17 21:30:09 +0800

    word_count

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    single_words = text.split()
+    words_number = word_count(single_words)
+    print(words_number)
+    
 
-if __name__ == "__main__":
-    main()
+def get_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def word_count(book):
+    words_total = 0
+    for i in range(0, len(book)):
+        words_total += 1
+    return words_total
+    
+
+
+
+main()
 
